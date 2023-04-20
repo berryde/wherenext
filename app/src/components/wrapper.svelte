7a6fa26cf7bfb0f2823ffdecd9ab9976e7f2d0f6
peter-hendy <57371348+peter-hendy@users.noreply.github.com>
2023-04-20 15:30:35 +0100

    map with zoom and padding

--- app/src/components/wrapper.svelte
+++ app/src/components/wrapper.svelte
@@ -1,13 +1,16 @@
 <script>
 	import { page } from '$app/stores';
 	import TabBar from './tab-bar.svelte';
+
+	export let margins=true;
 </script>
 
 <main
 	class="flex flex-col md:flex-row-reverse md:justify-end h-screen w-full bg-neutral-50 text-neutral-900"
 >
-	<section class="max-w-2xl h-full md:mx-auto flex-grow overflow-auto space-y-8 p-12">
+	<section class="{margins ? 'max-w-2xl p-12' : 'w-full'} h-full md:mx-auto flex-grow overflow-auto space-y-8">
 		<slot />
 	</section>
 	<TabBar selected={$page.route.id} />
 </main>
+
--- app/src/routes/map/+page.svelte
+++ app/src/routes/map/+page.svelte
@@ -1,7 +1,53 @@
 <script>
 	import Wrapper from '$components/wrapper.svelte';
+	import { geoPath, geoAlbersUsa } from 'd3-geo';
+	import { scaleSequential } from 'd3-scale';
+	import { interpolateBlues } from 'd3-scale-chromatic';
+	import { zoom } from 'd3-zoom';
+	import { select } from 'd3-selection';
+	import { onMount } from 'svelte';
+
+	$: width = 0;
+	$: height = 0;
+	$: geoJSON && render();
+
+	let geoJSON;
+	let path;
+	let svg;
+
+	onMount(() => {
+		fetchData();
+		render();
+	});
+
+	async function fetchData() {
+		const response = await fetch('/counties.json');
+		geoJSON = await response.json();
+		console.log(geoJSON);
+	}
+
+	function render() {
+		path = geoPath().projection(geoAlbersUsa().fitSize([width, height], geoJSON));
+		svg = select('#map').call(
+			zoom().on('zoom', (event) => {
+				svg.selectAll('g').attr('transform', event.transform);
+			})
+		);
+	}
 </script>
 
-<Wrapper>
+<Wrapper margins={false}>
 	<!-- TODO: add USA choropleth with D3 -->
+	<div bind:clientHeight={height} bind:clientWidth={width} class="w-full h-full">
+		<svg id="map" {width} {height}>
+			{#if geoJSON}
+				<g>
+					{#each geoJSON.features as feature}
+						<path d={path(feature)} fill={interpolateBlues(Math.random())}>
+						</path>
+					{/each}
+				</g>
+			{/if}
+		</svg>
+	</div>
 </Wrapper>
