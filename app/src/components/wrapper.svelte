76583bfcefd405905cd3814b9d29159d7fc53326
peter-hendy <57371348+peter-hendy@users.noreply.github.com>
2023-04-20 15:47:50 +0100

    fix: safari menubar

--- app/src/components/wrapper.svelte
+++ app/src/components/wrapper.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <main
-	class="flex flex-col md:flex-row-reverse md:justify-end h-screen w-full bg-neutral-50 text-neutral-900"
+	class="flex flex-col min-h-full md:flex-row-reverse md:justify-end h-screen w-full bg-neutral-50 text-neutral-900"
 >
 	<section class="{margins ? 'max-w-2xl p-12' : 'w-full'} h-full md:mx-auto flex-grow overflow-auto space-y-8">
 		<slot />
