79496bc04a712444564b8239947850d15f7520c3
Dan Berry <06berrydan@gmail.com>
2023-05-03 17:58:52 +0100

    feat: update landing image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,21 @@
 	import Subtitle from '$components/subtitle.svelte';
 	import Link from '$components/nav-link.svelte';
 	import Wrapper from '$components/wrapper.svelte';
-	import usa from '$lib/assets/usa.png';
+	import examples from '$lib/assets/examples.png';
 </script>
 
 <Wrapper menubar={false}>
-	<Title>WhereNext</Title>
-	<Subtitle>
-		Where's next? We help you choose your next location in the U.S. according to your needs with
-		just a few clicks
-	</Subtitle>
-	<img src={usa} alt="choropleth map of the USA" />
-	<Link href="/weights">Get started</Link>
+	<div class="py-10 md:py-20">
+		<Title>WhereNext</Title>
+		<Subtitle>
+			Where's next? We help you choose your next location in the U.S. according to your needs with
+			just a few clicks
+		</Subtitle>
+		<Link href="/weights">Get started</Link>
+		<img
+			src={examples}
+			alt="screenshots of the application"
+			class="px-5 md:max-w-2xl mx-auto py-5 md:py-10"
+		/>
+	</div>
 </Wrapper>
